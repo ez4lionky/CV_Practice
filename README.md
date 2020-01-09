605443ec7626694987b0b99cb536f505525188ac
ez4lionky <codex.lxy@gmail.com>
2020-01-09 17:06:25 +0800

    Initializing the repo by two examples from PCL.

---
+++ README.md
@@ -0,0 +1,3 @@
+## CV_Practice
+
+C/C++ code for CV practicing, using libraries such as PCL, OpenCV and etc.
---
+++ RT3dPointsReprojection/3DPointsReprojection.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/filter.h>
+
+#include <iostream>
